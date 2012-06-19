cf8565f7c0bee011d8e9d11787ca7ad15300e83c
prassee <prassee.sathian@gmail.com>
2012-06-19 23:17:26 +0530

    initial commit

---
+++ src/main/scala/scalafx/ensemble/Ensemble.scala
@@ -0,0 +1,102 @@
+package scalafx.ensemble
+
+import javafx.geometry.Pos
+import javafx.geometry.VPos
+import javafx.stage.StageStyle
+import scalafx.application.JFXApp
+import scalafx.scene.Scene._
+import scalafx.scene.control.Accordion._
+import scalafx.scene.control.Labeled._
+import scalafx.scene.control.TitledPane._
+import scalafx.scene.control.Label
+import scalafx.scene.image.Image
+import scalafx.scene.image.ImageView
+import scalafx.scene.layout.Pane._
+import scalafx.scene.layout.BorderPane
+import scalafx.scene.layout.HBox
+import scalafx.scene.shape.Circle._
+import scalafx.scene.text.Font
+import scalafx.scene.text.Text
+import scalafx.scene.Scene
+import scalafx.stage.Stage._
+import scalafx.stage.Stage
+import scalafx.geometry.Insets
+import javafx.scene.layout.Priority
+import scalafx.scene.layout.Region
+import scalafx.scene.control.Button
+import scalafx.scene.control.SplitPane
+import scalafx.scene.control.TabPane
+import scalafx.scene.control.Tab
+import scalafx.scene.control.TextField
+import scalafx.scene.control.TitledPane
+import scalafx.scene.shape.Arc
+import javafx.scene.paint.Color
+import scalafx.scene.control.Accordion
+import scalafx.scene.control.ListView
+
+object Ensemble extends JFXApp {
+
+  val tabs = new TabPane {
+    hgrow = javafx.scene.layout.Priority.ALWAYS
+  } += (new Tab {
+    text = "tab"
+    content = new TextField {
+      text = "text"
+    }
+  }) += (new Tab {
+    text = "tab"
+    content = new TextField {
+      text = "text"
+    }
+  }) += (new Tab {
+    text = "tab"
+    content = new TextField {
+      text = "text"
+    }
+  })
+
+  stage = new Stage {
+    scene = new Scene(1200, 768) {
+      content = new BorderPane {
+        hgrow = javafx.scene.layout.Priority.ALWAYS
+        vgrow = javafx.scene.layout.Priority.ALWAYS
+        top = new HBox {
+          content = List(
+            new ImageView {
+              image = new Image(this.getClass.getResourceAsStream("images/logo1.png"))
+              margin = Insets(0, 0, 0, 16)
+            },
+            new Region {
+              maxWidth = 900
+              margin = Insets(0, 0, 0, 160)
+            },
+            new Button {
+              minWidth = 120
+              minHeight = 66
+              id = "newButton"
+            })
+          minWidth = 1200
+          id = "mainToolBar"
+        }
+        left = new HBox {
+          title = "sidebar"
+          content =  List(new Accordion {
+            hgrow = javafx.scene.layout.Priority.ALWAYS
+            minWidth = 300
+            panes = List(
+              new TitledPane {
+                content = new ListView {
+                }
+              })
+          })
+        }
+        center = new BorderPane {
+          center = tabs
+        }
+        styleClass.add("application")
+      }
+    }
+    scene.get.getStylesheets.add(this.getClass.getResource("ensemble.css").toExternalForm)
+  }
+  stage.initStyle(StageStyle.UNDECORATED)
+}
