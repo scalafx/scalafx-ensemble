4068868a4776c83ed9d9d61b00f8f65b42e4299e
abuabdul <abuabdul86@hotmail.com>
2012-12-02 23:57:43 +0530

    Combo box is addes successfully

---
+++ src/main/scala/scalafx/ensemble/example/controls/EnsembleComboBox.scala
@@ -0,0 +1,41 @@
+package scalafx.ensemble.example.controls
+
+import scalafx.collections.ObservableBuffer
+import scalafx.collections.ObservableBuffer.observableBuffer2ObservableList
+import scalafx.ensemble.commons.EnsembleExample
+import scalafx.geometry.Insets
+import scalafx.scene.control.ComboBox
+import scalafx.scene.layout.VBox
+import scalafx.scene.text.Font
+import scalafx.scene.text.Text
+
+class EnsembleComboBox extends EnsembleExample {
+  def getContent = {
+    new VBox {
+      vgrow = javafx.scene.layout.Priority.ALWAYS
+      hgrow = javafx.scene.layout.Priority.ALWAYS
+      spacing = 10
+      margin = Insets(50, 0, 0, 50)
+      content = List(
+        new Text {
+          text = "Ensemble ComboBox"
+          font = new Font("Verdana", 20)
+        },
+        new ComboBox[String] {
+          maxWidth = 200
+          maxHeight = 50
+          promptText = "Choose a fruit..."
+          val seq = List("Apple", "Orange", "Mango", "Banana").toSeq
+          items = ObservableBuffer[String](seq)
+        },
+        new ComboBox[String] {
+          maxWidth = 200
+          maxHeight = 50
+          promptText = "Edit or Choose a fruit..."
+          editable = true
+          val seq = List("Apple", "Orange", "Mango", "Banana").toSeq
+          items = ObservableBuffer[String](seq)
+        })
+    }
+  }
+}
