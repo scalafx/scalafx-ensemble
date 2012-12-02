3e94a6da56d2f9ece4cb6ff342fb279e17b6e4c4
abuabdul <abuabdul86@hotmail.com>
2012-12-02 21:58:12 +0530

    Label src is lost and i am commiting it again

--- src/main/scala/scalafx/ensemble/example/controls/EnsembleLabel.scala
+++ src/main/scala/scalafx/ensemble/example/controls/EnsembleLabel.scala
@@ -1,21 +1,29 @@
 package scalafx.ensemble.example.controls
 
 import scalafx.ensemble.commons.EnsembleExample
-import scalafx.scene.layout.VBox
+import scalafx.geometry.Insets
 import scalafx.scene.control.Label
+import scalafx.scene.layout.VBox
 import scalafx.scene.text.Font
+import scalafx.scene.text.Font.sfxFont2jfx
 
 class EnsembleLabel extends EnsembleExample {
   def getContent = {
     new VBox {
       vgrow = javafx.scene.layout.Priority.ALWAYS
       hgrow = javafx.scene.layout.Priority.ALWAYS
+      spacing = 10
+      margin = Insets(50, 0, 0, 50)
       style = "-fx-padding: 8px"
       content = List(
         new Label {
-          text = "This is Label for sample display"
-          font = new Font("Sans-serif", 13)
-          style = "-fx-font-weight: bold;-fx-font-color: blue;"
+          text = "Ensemble Label"
+          font = new Font("Verdana", 20)
+        },
+        new Label {
+          text = "Ensemble Label with styles"
+          font = new Font("Verdana", 20)
+          style = "-fx-font-weight: bold"
         })
     }
   }
