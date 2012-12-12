9b75bd9b9cc9b4d87fea8e506dc2285a666c3fc1
abuabdul <abuabdul86@hotmail.com>
2012-12-12 21:55:44 +0530

    Path, polygon, polyline and QuadCurve added.

---
+++ src/main/scala/scalafx/ensemble/example/shapes/EnsemblePath.scala
@@ -0,0 +1,54 @@
+package scalafx.ensemble.example.shapes
+
+import scalafx.ensemble.commons.EnsembleExample
+import scalafx.geometry.Insets
+import scalafx.scene.control.Label
+import scalafx.scene.layout.VBox
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.ArcTo
+import scalafx.scene.shape.ArcTo.sfxArcTo2jfx
+import scalafx.scene.shape.ClosePath
+import scalafx.scene.shape.ClosePath.sfxClosePath2jfx
+import scalafx.scene.shape.CubicCurveTo
+import scalafx.scene.shape.CubicCurveTo.sfxCubicCurveTo2jfx
+import scalafx.scene.shape.HLineTo
+import scalafx.scene.shape.HLineTo.sfxHLineTo2jfx
+import scalafx.scene.shape.LineTo
+import scalafx.scene.shape.LineTo.sfxLineTo2jfx
+import scalafx.scene.shape.MoveTo
+import scalafx.scene.shape.MoveTo.sfxMoveTo2jfx
+import scalafx.scene.shape.Path
+import scalafx.scene.shape.QuadCurveTo
+import scalafx.scene.shape.QuadCurveTo.sfxQuadCurveTo2jfx
+import scalafx.scene.shape.VLineTo
+import scalafx.scene.shape.VLineTo.sfxVLineTo2jfx
+import scalafx.scene.text.Font
+
+class EnsemblePath extends EnsembleExample {
+  def getContent = {
+    new VBox {
+      vgrow = javafx.scene.layout.Priority.ALWAYS
+      hgrow = javafx.scene.layout.Priority.ALWAYS
+      spacing = 10
+      margin = Insets(50, 0, 0, 50)
+      content = List(
+        new Label {
+          text = "Ensemble Path"
+          font = new Font("Verdana", 20)
+        },
+        new Path {
+          elements = List(MoveTo(35, 35), HLineTo(75), VLineTo(75), LineTo(35, 75), new ClosePath())
+          fill = Color.ORANGE
+          stroke = Color.BLACK
+          strokeWidth = 1d
+        },
+        new Path {
+          elements = List(MoveTo(100, 55),
+            CubicCurveTo(120, 20, 130, 80, 140, 45),
+            QuadCurveTo(150, 0, 160, 45),
+            ArcTo(20, 40, 0, 180, 45, true, true))
+          fill = Color.AZURE
+        })
+    }
+  }
+}
---
+++ src/main/scala/scalafx/ensemble/example/shapes/EnsembleQuadCurve.scala
@@ -0,0 +1,55 @@
+package scalafx.ensemble.example.shapes
+
+import scalafx.ensemble.commons.EnsembleExample
+import scalafx.geometry.Insets
+import scalafx.scene.control.Label
+import scalafx.scene.layout.VBox
+import scalafx.scene.paint.Color
+import scalafx.scene.paint.Color.sfxColor2jfx
+import scalafx.scene.paint.Paint
+import scalafx.scene.paint.Paint.sfxPaint2jfx
+import scalafx.scene.shape.Circle
+import scalafx.scene.shape.Circle.sfxCircle2jfx
+import scalafx.scene.shape.CubicCurve
+import scalafx.scene.shape.CubicCurve.sfxCubicCurve2jfx
+import scalafx.scene.text.Font
+import scalafx.scene.text.Font.sfxFont2jfx
+import scalafx.scene.shape.QuadCurve
+
+class EnsembleQuadCurve extends EnsembleExample {
+  def getContent = {
+    new VBox {
+      vgrow = javafx.scene.layout.Priority.ALWAYS
+      hgrow = javafx.scene.layout.Priority.ALWAYS
+      spacing = 10
+      margin = Insets(50, 0, 0, 50)
+      content = List(
+        new Label {
+          text = "Ensemble Quad Curve"
+          font = new Font("Verdana", 20)
+        },
+        new QuadCurve {
+          controlX = 50
+          controlY = 10
+          startX = 0
+          startY = 45
+          endX = 180
+          endY = 45
+          stroke = Color.RED
+          fill = Color.ROSYBROWN
+          strokeWidth = 2d
+        },
+        new QuadCurve {
+          controlX = 150
+          controlY = 100
+          startX = 35
+          startY = 145
+          endX = 210
+          endY = 145
+          stroke = Color.BLUE
+          fill = Color.WHITE
+          strokeWidth = 2d
+        })
+    }
+  }
+}
