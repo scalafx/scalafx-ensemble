fd831ffc77e9b6b2bdde809e5512949ab0703c62
Alain Bearez <A@cua.li>
2013-02-06 20:15:02 -0300

    Updated to ScalaFX current status.

--- src/main/scala/scalafx/ensemble/example/controls/EnsembleRadioButton.scala
+++ src/main/scala/scalafx/ensemble/example/controls/EnsembleRadioButton.scala
@@ -4,6 +4,7 @@ import scalafx.ensemble.commons.EnsembleExample
 import scalafx.geometry.Insets
 import scalafx.scene.control.RadioButton
 import scalafx.scene.control.ToggleGroup
+import scalafx.scene.layout.Priority
 import scalafx.scene.layout.VBox
 import scalafx.scene.text.Font
 import scalafx.scene.text.Text
@@ -11,8 +12,8 @@ import scalafx.scene.text.Text
 class EnsembleRadioButton extends EnsembleExample {
   def getContent = {
     new VBox {
-      vgrow = javafx.scene.layout.Priority.ALWAYS
-      hgrow = javafx.scene.layout.Priority.ALWAYS
+      vgrow = Priority.ALWAYS
+      hgrow = Priority.ALWAYS
       spacing = 10
       margin = Insets(50, 0, 0, 50)
       
