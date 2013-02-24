15ff028d6e2e1e18ad335c064b6324f97dcb63d8
abuabdul <abuabdul86@hotmail.com>
2013-02-24 17:24:11 +0530

    copy right is added

--- src/main/scala/scalafx/ensemble/example/controls/EnsemblePagination.scala
+++ src/main/scala/scalafx/ensemble/example/controls/EnsemblePagination.scala
@@ -1,18 +1,47 @@
+/*
+ * Copyright (c) 2012, ScalaFX Ensemble Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package scalafx.ensemble.example.controls
 
+import javafx.util.Callback
 import scalafx.ensemble.commons.EnsembleExample
 import scalafx.geometry.Insets
 import scalafx.scene.Node
 import scalafx.scene.control.Button
+import scalafx.scene.control.Control
 import scalafx.scene.control.Label
 import scalafx.scene.control.Pagination
+import scalafx.scene.control.Pagination.sfxPagination2jfx
 import scalafx.scene.image.Image
 import scalafx.scene.image.ImageView
 import scalafx.scene.layout.Priority
 import scalafx.scene.layout.VBox
 import scalafx.scene.text.Font
 import scalafx.scene.text.Text
-import scalafx.scene.control.Control
 
 class EnsemblePagination extends EnsembleExample {
   def getContent = {
@@ -25,14 +54,14 @@ class EnsemblePagination extends EnsembleExample {
     val pagination = new Pagination(7, 0)
     val indexToNode = (index: Int) => {
       val vbox = new VBox() {
-        content = List(new ImageView(images(index)),new Label("PAGE " + (index + 1)))
+        content = List(new ImageView(images(index)), new Label("PAGE " + (index + 1)))
       }.asInstanceOf[Node]
       vbox
     }
-    
-    //    pagination.pageFactory = indexToNode
-    /*pagination.setPageFactory(new Callback[java.lang.Integer, javafx.scene.Node]() {
-      override def call(index: java.lang.Integer) {
+
+    /*pagination.pageFactory = indexToNode
+    pagination.setPageFactory(new Callback[java.lang.Integer, javafx.scene.Node]() {
+      override def call(index: Int) {
         new VBox() {
           content = List(
             new ImageView(images(index)),
@@ -40,9 +69,8 @@ class EnsemblePagination extends EnsembleExample {
           println("Inside Pagination " + index)
         }.asInstanceOf[javafx.scene.Node]
       }
-    })
-*/
-    
+    })*/
+
     //TODO pagination incomplete
     new VBox {
       vgrow = Priority.ALWAYS
--- src/main/scala/scalafx/ensemble/example/shapes/EnsemblePath.scala
+++ src/main/scala/scalafx/ensemble/example/shapes/EnsemblePath.scala
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2012, ScalaFX Ensemble Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the ScalaFX Project nor the
+ *       names of its contributors may be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE SCALAFX PROJECT OR ITS CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package scalafx.ensemble.example.shapes
 
 import scalafx.Includes._
@@ -7,22 +34,14 @@ import scalafx.scene.control.Label
 import scalafx.scene.layout.VBox
 import scalafx.scene.paint.Color
 import scalafx.scene.shape.ArcTo
-//import scalafx.scene.shape.ArcTo.sfxArcTo2jfx
 import scalafx.scene.shape.ClosePath
-//import scalafx.scene.shape.ClosePath.sfxClosePath2jfx
 import scalafx.scene.shape.CubicCurveTo
-//import scalafx.scene.shape.CubicCurveTo.sfxCubicCurveTo2jfx
 import scalafx.scene.shape.HLineTo
-//import scalafx.scene.shape.HLineTo.sfxHLineTo2jfx
 import scalafx.scene.shape.LineTo
-//import scalafx.scene.shape.LineTo.sfxLineTo2jfx
 import scalafx.scene.shape.MoveTo
-//import scalafx.scene.shape.MoveTo.sfxMoveTo2jfx
 import scalafx.scene.shape.Path
 import scalafx.scene.shape.QuadCurveTo
-//import scalafx.scene.shape.QuadCurveTo.sfxQuadCurveTo2jfx
 import scalafx.scene.shape.VLineTo
-//import scalafx.scene.shape.VLineTo.sfxVLineTo2jfx
 import scalafx.scene.text.Font
 
 class EnsemblePath extends EnsembleExample {
