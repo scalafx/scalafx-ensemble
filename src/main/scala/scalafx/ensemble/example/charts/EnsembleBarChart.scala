256193d0c0060ef410abbeb31396e87ee03dbe4c
hrj <harshad.rj@gmail.com>
2013-05-23 00:04:42 +0530

    Simplification of bar chart code

--- src/main/scala/scalafx/ensemble/example/charts/EnsembleBarChart.scala
+++ src/main/scala/scalafx/ensemble/example/charts/EnsembleBarChart.scala
@@ -51,52 +51,50 @@ class EnsembleBarChart extends EnsembleExample {
           text = "Ensemble Bar Chart"
           font = new Font("Verdana", 20)
         },
-        createBarChart)
+        barChart)
     }
   }
 
-  val createBarChart = {
-    val years = new Array[String](3)
-    years(0) = "2007"
-    years(1) = "2008"
-    years(2) = "2009"
+  lazy val barChart = {
+    val years = Array("2007", "2008", "2009")
 
     val xAxis = new CategoryAxis()
-    xAxis.setCategories(ObservableBuffer[String](years.toSeq))
+    xAxis.setCategories(ObservableBuffer(years.toSeq))
 
     val yAxis = NumberAxis("Units Sold", 0.0d, 3000.0d, 1000.0d)
 
     val bcSeries1 = new XYChart.Series[String, Number]()
     bcSeries1.setName("Apples")
     // create sample data
-    bcSeries1.getData().add(XYChart.Data[String, Number](years(0), 567d))
-    bcSeries1.getData().add(XYChart.Data[String, Number](years(1), 1292d))
-    bcSeries1.getData().add(XYChart.Data[String, Number](years(2), 1292d))
+    val sampleData1 = Seq(567d, 1292d, 1292d)
+    sampleData1.zip(years).foreach { case (data, year) =>
+      bcSeries1.getData().add(XYChart.Data[String, Number](year, data))
+    }
 
     val bcSeries2 = new XYChart.Series[String, Number]()
     bcSeries2.setName("Lemons")
-    /*
-     * create sample data
-     */
-    bcSeries2.getData().add(XYChart.Data[String, Number](years(0), 956))
-    bcSeries2.getData().add(XYChart.Data[String, Number](years(1), 1665))
-    bcSeries2.getData().add(XYChart.Data[String, Number](years(2), 2559))
+    // create sample data
+    val sampleData2 = Seq(956, 1665, 2559)
+    sampleData2.zip(years).foreach { case (data, year) =>
+      bcSeries2.getData().add(XYChart.Data[String, Number](year, data))
+    }
 
     val bcSeries3 = new XYChart.Series[String, Number]()
     bcSeries3.setName("Oranges")
-    /*
-     * create sample data
-     */
-    bcSeries3.getData().add(XYChart.Data[String, Number](years(0), 1154))
-    bcSeries3.getData().add(XYChart.Data[String, Number](years(1), 1927))
-    bcSeries3.getData().add(XYChart.Data[String, Number](years(2), 2774))
+    // create sample data
+    val sampleData3 = Seq(1154, 1927, 2774)
+    sampleData3.zip(years).foreach { case (data, year) =>
+      bcSeries3.getData().add(XYChart.Data[String, Number](year, data))
+    }
 
     //Bar Chart
     val barChart = BarChart[String, Number](xAxis, yAxis)
-    barChart.getData.add(bcSeries1)
-    barChart.getData.add(bcSeries2)
-    barChart.getData.add(bcSeries3)
+    val chartData = barChart.getData
+    Seq(bcSeries1, bcSeries2, bcSeries3).foreach {series =>
+      chartData.add(series)
+    }
     barChart.setCategoryGap(25.0d)
     barChart
+
   }
 }
