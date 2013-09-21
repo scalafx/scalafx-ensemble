1d4d4470d8fbbdf2b6d20836f6f57b63cfd65fdb
Jarek Sacha <jpsacha@gmail.com>
2013-09-20 21:10:21 -0400

    Add syntax highlighting to the displayed source code.

--- src/main/scala/scalafx/ensemble/commons/EnsembleCommons.scala
+++ src/main/scala/scalafx/ensemble/commons/EnsembleCommons.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, ScalaFX Ensemble Project
+ * Copyright (c) 2013, ScalaFX Ensemble Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,27 +26,19 @@
  */
 package scalafx.ensemble.commons
 
-import java.io.BufferedReader
-import java.io.IOException
-import java.io.InputStreamReader
-import java.net.URISyntaxException
+import java.util.Locale
 
-import scalafx.Includes.jfxPriority2sfx
 import scalafx.ensemble.EnsembleThumbNail
 import scalafx.ensemble.stage.DashboardPage
 import scalafx.ensemble.stage.EnsembleTabbedPage
 import scalafx.scene.Node
-import scalafx.scene.Node.sfxNode2jfx
-import scalafx.scene.control.Label.sfxLabel2jfx
 import scalafx.scene.control.ScrollPane
-import scalafx.scene.control.ScrollPane.sfxScrollPane2jfx
 import scalafx.scene.control.TreeItem
-import scalafx.scene.control.TreeItem.sfxTreeItemToJfx
 import scalafx.scene.layout.BorderPane
-import scalafx.scene.layout.BorderPane.sfxBorderPane2jfx
 import scalafx.scene.layout.Priority
 import scalafx.scene.layout.VBox
 import scalafx.scene.web.WebView
+
 
 /**
  * the class that updates tabbed view or dashboard view
@@ -61,7 +53,7 @@ object PageDisplayer {
       }
       case _ => {
         if (value.startsWith("dashBoard - ")) {
-          displayPage(new DashboardPage((value.split("-")(1)).trim()))
+          displayPage(new DashboardPage(value.split("-")(1).trim()))
         } else {
           displayPage(EnsembleTabbedPage.buildTab(value.split(">")(1).trim(), value.split(">")(0).trim()))
         }
@@ -87,10 +79,10 @@ object PageDisplayer {
 object SortUtils {
 
   def treeItemSort = (ti: TreeItem[String], t2: TreeItem[String]) =>
-    compare(ti.getValue(), t2.getValue())
+    compare(ti.value(), t2.value())
 
   def thumbNailsSort = (t1: EnsembleThumbNail, t2: EnsembleThumbNail) =>
-    compare(t1.caption.getText(), t2.caption.getText())
+    compare(t1.caption.text(), t2.caption.text())
 
   def sortKeys = (x: String, y: String) => compare(x, y)
 
@@ -124,63 +116,60 @@ object ContentFactory {
       minWidth = 725
       content = borderPane
     }
-    scrollPane.getStyleClass().add("noborder-scroll-pane")
+    scrollPane.styleClass.add("noborder-scroll-pane")
     scrollPane
   }
 
-  
+
   def createSrcContent(ctrlName: String, ctrlgroupName: String = "") = {
-    val htmlpage = """<html><head></head><body><div style="font-size: 13px; font-family: sans-serif; white-space:pre;">"""
 
-    val createDiv = (src:String) => {
-      """<div style="font-size: 13px; font-family: sans-serif; color: green; white-space:pre;">""" + src + """</div>"""
-    }
-    
-    val createOpenDiv = (src: String)=> {
-      """<div style="font-size: 13px; font-family: sans-serif; color: green; white-space:pre;">""" + src + """<br/>"""
-    }
-    //read function to read the file content one by one 
-    val readSrc = (s: String, builder: StringBuilder) => {
-      s match {
-        case null => false
-        case _ => {
-          if (s.contains("//")) {
-            builder.append(createDiv(s))
-          } else if (s.contains("/*")) {
-            builder.append(createOpenDiv(s))
-          } else if (s.contains("*/")) {
-            builder.append(s);
-            builder.append("</div>")
-          } else {
-            builder.append(s);
-            builder.append("<br/>")
-          }
-          true
-        }
-      }
-    }
-    // File to read src file
-    val file = this.getClass.getResource(
-      "/scalafx/ensemble/example/" + ctrlgroupName + "/Ensemble" + ctrlName + ".scala")
-    // Stringbuilder to store src code lines
-    val builder = new StringBuilder().append(htmlpage)
+    // Load syntax highlighter
+    val shCoreJs = loadResource("/scalafx/ensemble/syntaxhighlighter/shCore.js") + ";"
+    val shBrushScala = loadResource("/scalafx/ensemble/syntaxhighlighter/shBrushScala.js")
+    val shCoreDefaultCss = loadResource("/scalafx/ensemble/syntaxhighlighter/shCoreDefault.css")
 
-    try {
-      // load src into String
-      val in = file.openStream()
-      val reader = new BufferedReader(new InputStreamReader(in))
-      var isContent = true
-      do {
-        isContent = readSrc(reader.readLine(), builder)
-      } while (isContent)
-      reader.close()
-    } catch {
-      case cnfe: ClassNotFoundException => cnfe.printStackTrace()
-      case urie: URISyntaxException => urie.printStackTrace()
-      case ioe: IOException => ioe.printStackTrace()
-    }
-    // Complete the html content
-    builder.append("</p></body></html>")
+    // Load source code text
+    val sourceRaw = loadResource("/scalafx/ensemble/example/" + ctrlgroupName + "/Ensemble" + ctrlName + ".scala")
+
+    // Remove initial comment
+    val source = sourceRaw.replaceFirst( """(?s)/\*(.*?)\*/""", "")
+
+    // Create HTML, for now do not embed SyntaxHighlighter scripts to avoid issues with auto-escaping,
+    // just put placeholders @@...@@
+    val codeFont = if (isMac) "Ayuthaya" else "monospace"
+    val html =
+      <html>
+        <head>
+          <script type="text/javascript">
+            @@shCoreJs@@
+            @@shBrushScala@@
+          </script>
+          <style>
+            @@shCoreDefaultCss@@
+            .syntaxhighlighter {{
+            overflow: visible;
+            font: 12px
+            {codeFont}
+            !important; line-height: 150% !important;
+            }}
+            code {{ font: 12px
+            {codeFont}
+            !important; line-height: 150% !important; }}
+          </style>
+        </head>
+        <body>
+          <pre class="brush: scala;gutter: false;toolbar: false;">
+            {"\n" + source}
+          </pre>
+          <script type="text/javascript">SyntaxHighlighter.all();</script>
+        </body>
+      </html>
+
+    // Inject SyntaxHighlighter scripts
+    val htmlString = html.mkString.
+      replace("@@shCoreJs@@", shCoreJs).
+      replace("@@shBrushScala@@", shBrushScala).
+      replace("@@shCoreDefaultCss@@", shCoreDefaultCss)
 
     //Border pane is sufficient to handle the content
     val borderPane = new BorderPane() {
@@ -188,11 +177,22 @@ object ContentFactory {
       center = new WebView() {
         contextMenuEnabled = false
         prefWidth = 300
-        this.engine.loadContent(builder.mkString)
+        this.engine.loadContent(htmlString)
       }
     }
 
     borderPane
+  }
+
+
+  def loadResource(path: String): String = {
+    val in = this.getClass.getResourceAsStream(path)
+    scala.io.Source.fromInputStream(in).mkString
+  }
+
+  def isMac: Boolean = {
+    val os = System.getProperty("os.name").toLowerCase(new Locale(""))
+    os.indexOf("mac") >= 0
   }
 }
 
