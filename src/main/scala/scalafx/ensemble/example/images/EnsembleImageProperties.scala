00a4f0be6e0fa830cb716ad4df8c804f89ad0600
Jarek Sacha <jpsacha@gmail.com>
2013-10-19 23:23:20 -0400

    Cleanup ImageProperties example and add  updated thumbnail icon.

--- src/main/scala/scalafx/ensemble/example/images/EnsembleImageProperties.scala
+++ src/main/scala/scalafx/ensemble/example/images/EnsembleImageProperties.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, ScalaFX Ensemble Project
+ * Copyright (c) 2012-2013, ScalaFX Ensemble Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,63 +27,70 @@
 
 package scalafx.ensemble.example.images
 
-import scalafx.Includes._
 import scalafx.ensemble.commons.EnsembleExample
 import scalafx.geometry.Insets
 import scalafx.geometry.Rectangle2D
-import scalafx.scene.control.Label
 import scalafx.scene.image.Image
 import scalafx.scene.image.ImageView
 import scalafx.scene.layout.HBox
-import scalafx.scene.layout.VBox
-import scalafx.scene.text.Font
 import scalafx.scene.layout.Priority
+import scalafx.scene.layout.VBox
 
+/** A sample that demonstrates how to resize images and use the Viewport property.
+  *
+  * @related images/ImageCreation
+  * @see scalafx.scene.image.Image
+  * @see scalafx.scene.image.ImageView
+  * @resource /scalafx/ensemble/images/sanfran.jpg
+  */
 class EnsembleImageProperties extends EnsembleExample {
+
   def getContent = {
-    //we can set image properties directly during creation
-    val url = this.getClass.getResource("/scalafx/ensemble/images/sanfran.jpg").toExternalForm()
-    val sample1 = new ImageView(new Image(url, 30, 70, false, true))
+    // We can set image properties directly during creation
+    val url = this.getClass.getResource("/scalafx/ensemble/images/sanfran.jpg").toExternalForm
+    val sample1 = new ImageView(
+      new Image(url, requestedWidth = 30, requestedHeight = 70, preserveRatio = false, smooth = true))
 
-    val sample2 = new ImageView(new Image(url))
-    //image can be resized to preferred width
-    sample2.setFitWidth(200)
-    sample2.setPreserveRatio(true)
+    val sample2 = new ImageView(new Image(url)) {
+      // Image can be resized to preferred width
+      fitWidth = 200
+      preserveRatio = true
+    }
 
-    val sample3 = new ImageView(new Image(url))
-    //image can be resized to preferred height
-    sample3.setFitHeight(20);
-    sample3.setPreserveRatio(true);
+    val sample3 = new ImageView(new Image(url)) {
+      //image can be resized to preferred height
+      fitHeight = 20
+      preserveRatio = true
+    }
 
-    val sample4 = new ImageView(new Image(url))
-    //one can resize image without preserving ratio between height and width
-    sample4.setFitWidth(40)
-    sample4.setFitHeight(80)
-    sample4.setPreserveRatio(false)
-    sample4.setSmooth(true) //the usage of the better filter
+    val sample4 = new ImageView(new Image(url)) {
+      // One can resize image without preserving ratio between height and width
+      fitWidth = 40
+      fitHeight = 80
+      preserveRatio = false
+      // The usage of the better filter
+      smooth = true
+    }
 
-    val sample5 = new ImageView(new Image(url))
-    sample5.setFitHeight(60)
-    sample5.setPreserveRatio(true)
-    //viewport is used for displaying the part of image
-    val rectangle2D = new Rectangle2D(50, 200, 120, 60);
-    sample5.setViewport(rectangle2D)
+    val sample5 = new ImageView(new Image(url)) {
+      fitHeight = 60
+      preserveRatio = true
+      // Viewport is used for displaying the part of image
+      viewport = new Rectangle2D(50, 200, width = 120, height = 60)
+    }
 
     new VBox {
       vgrow = Priority.ALWAYS
       hgrow = Priority.ALWAYS
       spacing = 10
-      margin = Insets(50, 0, 0, 50)
+      padding = Insets(20)
       content = List(
-        new Label {
-          text = "Ensemble Image Properties"
-          font = new Font("Verdana", 20)
-        },
         new HBox {
           spacing = 5
           content = List(sample1, sample3, sample4, sample5)
         },
-        sample2)
+        sample2
+      )
     }
   }
 }
