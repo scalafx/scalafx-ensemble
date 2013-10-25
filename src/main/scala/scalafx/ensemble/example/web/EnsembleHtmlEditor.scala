951aca32e260ad56cba59c0d593bb5b207c40dc3
Jarek Sacha <jpsacha@gmail.com>
2013-10-24 22:57:25 -0400

    Update web examples.

--- src/main/scala/scalafx/ensemble/example/web/EnsembleHtmlEditor.scala
+++ src/main/scala/scalafx/ensemble/example/web/EnsembleHtmlEditor.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, ScalaFX Ensemble Project
+ * Copyright (c) 2012-2013, ScalaFX Ensemble Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -28,68 +28,53 @@
 package scalafx.ensemble.example.web
 
 import scalafx.Includes._
-import scalafx.event.ActionEvent
-import scalafx.event.EventHandler
 import scalafx.ensemble.commons.EnsembleExample
+import scalafx.event.ActionEvent
 import scalafx.geometry.Insets
 import scalafx.scene.control.Button
 import scalafx.scene.control.Label
 import scalafx.scene.control.ScrollPane
-import scalafx.scene.layout.VBox
-import scalafx.scene.text.Font
+import scalafx.scene.layout.{Priority, VBox}
 import scalafx.scene.web.HTMLEditor
-import scalafx.scene.layout.Priority
 
 class EnsembleHtmlEditor extends EnsembleExample {
+
   def getContent = {
     // Initial Text in the html editor
     val initialText = """<html><body>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-
-            Nam tortor felis, pulvinar in scelerisque cursus, pulvinar at ante. Nulla consequat 
-
-            congue lectus in sodales. Nullam eu est a felis ornare bibendum et nec tellus. 
-
-            Vivamus non metus tempus augue auctor ornare. Duis pulvinar justo ac purus adipiscing 
-
-            pulvinar. Integer congue faucibus dapibus. Integer id nisl ut elit aliquam sagittis 
-
+            Nam tortor felis, pulvinar in scelerisque cursus, pulvinar at ante. Nulla consequat
+            congue lectus in sodales. Nullam eu est a felis ornare bibendum et nec tellus.
+            Vivamus non metus tempus augue auctor ornare. Duis pulvinar justo ac purus adipiscing
+            pulvinar. Integer congue faucibus dapibus. Integer id nisl ut elit aliquam sagittis
             gravida eu dolor. Etiam sit amet ipsum sem.</body></html>"""
 
     val htmlEditor = new HTMLEditor {
-      maxWidth = 400
-      maxHeight = 250
+      prefHeight = 250
+      hgrow = Priority.ALWAYS
+      vgrow = Priority.ALWAYS
       htmlText = initialText
     }
 
     val htmlLabel = new Label {
-      maxWidth = 300
       wrapText = true
     }
 
-    val scrollPane = new ScrollPane {
-      content = htmlLabel
-      maxWidth = 400
-      maxHeight = 350
-    }
-
     new VBox {
-      vgrow = Priority.ALWAYS
-      hgrow = Priority.ALWAYS
-      spacing = 10
-      margin = Insets(50, 0, 0, 50)
+      spacing = 5
+      padding = Insets(5)
       content = List(
-        new Label {
-          text = "Ensemble HTML Editor"
-          font = new Font("Verdana", 20)
-        },
         htmlEditor,
         new Button {
           text = "Display Html below"
-          onAction = (ae: ActionEvent) => {
-              htmlLabel.setText(htmlEditor.getHtmlText())
-          }
+          onAction = (ae: ActionEvent) => htmlLabel.text = htmlEditor.htmlText
         },
-        scrollPane)
+        new ScrollPane {
+          prefHeight = 200
+          hgrow = Priority.ALWAYS
+          vgrow = Priority.ALWAYS
+          content = htmlLabel
+        }
+      )
     }
   }
 }
--- src/main/scala/scalafx/ensemble/example/web/EnsembleWebView.scala
+++ src/main/scala/scalafx/ensemble/example/web/EnsembleWebView.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, ScalaFX Ensemble Project
+ * Copyright (c) 2012-2013, ScalaFX Ensemble Project
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,85 +27,51 @@
 
 package scalafx.ensemble.example.web
 
-import javafx.beans.value.ObservableValue
 import scalafx.Includes._
-import scalafx.event.ActionEvent
-import scalafx.event.EventHandler
 import scalafx.ensemble.commons.EnsembleExample
+import scalafx.event.ActionEvent
 import scalafx.geometry.Insets
 import scalafx.scene.control.Button
-import scalafx.scene.control.Label
 import scalafx.scene.control.TextField
-import scalafx.scene.layout.HBox
-import scalafx.scene.layout.VBox
-import scalafx.scene.text.Font
+import scalafx.scene.layout.{BorderPane, Priority, HBox}
 import scalafx.scene.web.WebView
-import scalafx.scene.layout.Priority
 
 class EnsembleWebView extends EnsembleExample {
-  def getContent = {
-    //Default URL to load at first
-    val DEFAULT_URL = "http://www.scala-lang.org/";
-    //Webview control
-    val webView = new WebView {
-      maxWidth = 1000
-      maxHeight = 500
-    }
-    val webEngine = webView.getEngine()
-    webEngine.load(DEFAULT_URL)
+  // @stage-property width = 1020
+  // @stage-property height = 700
 
+  def getContent = {
+    // Default URL to load at first
+    val defaultURL = "http://www.scala-lang.org/"
+    val locationField = new TextField {
+      text = defaultURL
+      hgrow = Priority.ALWAYS
+    }
     val goButton = new Button {
       text = "Go"
       defaultButton = true
     }
-
-    val textUrl = new TextField {
-      text = DEFAULT_URL
-      prefWidth = 250
+    val webView = new WebView {
+      // Update location field is page is redirected
+      engine.location.onChange((_, _, newValue) => locationField.setText(newValue))
+      // Load default page
+      engine.load(defaultURL)
     }
-    // Function literal matching textfield contains http or not
-    val validUrl = (url: String) => {
-      url match {
-        case url if url.contains("http://") => url
-        case _ => "http://" + textUrl.getText()
+
+    def validUrl(url: String) = if (url.startsWith("http://")) url else "http://" + locationField.text()
+
+    val loadAction = (ae: ActionEvent) => webView.engine.load(validUrl(locationField.text()))
+    goButton.onAction = loadAction
+    locationField.onAction = loadAction
+
+    new BorderPane {
+      padding = Insets(5)
+      top = new HBox {
+        spacing = 5
+        margin = Insets(top = 0, right = 0, bottom = 5, left = 0)
+        content = List(locationField, goButton)
       }
-    }
-
-    val loadAction = (ae: ActionEvent) => {
-        webEngine.load(validUrl(textUrl.getText()))
-    }
-    //On action is set for textfield if Enter key is pressed
-    textUrl onAction = loadAction
-    goButton onAction = loadAction
-
-    /*//TextField is given horizontal priority
-    HBox.setHgrow(textUrl, Priority.ALWAYS)*/
-
-    //WebEngine Location Property is added with changeListener for textfield value changes
-    webEngine.locationProperty.addListener(
-      (observable: ObservableValue[_], oldValue: Any, newValue: Any) => {
-        val newTextVal = newValue.asInstanceOf[String]
-        textUrl.setText(newTextVal)
-      }
-    )
-
-    new VBox {
-      vgrow = Priority.ALWAYS
-      hgrow = Priority.ALWAYS
-      spacing = 10
-      margin = Insets(50, 0, 0, 50)
-      content = List(
-        new Label {
-          text = "Ensemble Web View"
-          font = new Font("Verdana", 20)
-        },
-        new HBox {
-          spacing = 3
-          content = List(
-            textUrl,
-            goButton)
-        },
-        webView)
+      center = webView
     }
   }
 }
